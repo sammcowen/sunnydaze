aed1e40bc0cce867e844e2dfb7fb0d662ab82f15
HBK <sammcowen@gmail.com>
2022-07-07 12:43:06 -0700

    did some basic styling and spacing of the nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
 import Header from './Components/Header/Header';
-
+import AboutMe from './Sections/AboutMe/AboutMe';
+import MyWork from './Sections/AboutMe/AboutMe';
 function App() {
   return (
     <div className="App">
       <Header/>
+      <AboutMe/>
+      <MyWork/>
     </div>
   );
 }
