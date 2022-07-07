f1945ead225a892d27193afb72edb8b3a3f123a3
HBK <sammcowen@gmail.com>
2022-07-07 12:38:36 -0700

    pplacing filler content into the different sections of rportfolio

---
+++ src/Sections/AboutMe/AboutMe.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const AboutMe = () => {
+  return (
+    <section id='aboutMeSection'>
+        <h2>About Me</h2>
+        <p>Lore abdkiashdashdj ahsdhsaduhus asgduadj 
+            adhuwefhujenjsghcbf ahdbguagdbjad badgugduahd bjdjebfjuefgb badgughdujbas d 
+        </p>
+    </section>
+    
+  )
+}
+
+export default AboutMe
---
+++ src/Sections/MyWork/MyWork.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const MyWork = () => {
+  return (
+    <section id='myWorkSection'>
+        <h2>My Work</h2>
+        <p>Lore abdkiashdashdj ahsdhsaduhus asgduadj 
+            adhuwefhujenjsghcbf ahdbguagdbjad badgugduahd bjdjebfjuefgb badgughdujbas d 
+        </p>
+    </section>
+    
+  )
+}
+
+export default MyWork
