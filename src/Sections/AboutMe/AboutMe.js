9396155c6eb86d3d383669439c9a67aaa30c522d
HBK <sammcowen@gmail.com>
2022-07-08 22:11:30 -0700

    cards finally all same size

--- src/Sections/AboutMe/AboutMe.js
+++ src/Sections/AboutMe/AboutMe.js
@@ -9,7 +9,7 @@ const AboutMe = () => {
       </div>
         <h2 >Hello.<br/>
        <span>I am</span> Samm </h2>
-        <p class ='sectionHeader'> <span>Full Stack </span>  Web Developer . B.A. in <span> Communications.</span>I love making <span>magic</span> with my code to create <span>useful</span>  and <span>visually pleasing</span> websites.<br/>
+        <p class ='sectionHeader'> <span>Full Stack </span>  Web Developer . B.A. in <span> Communications.</span>I love making <span>magic</span> with my code to create  <span>web responsive</span> designs.<br/>
         Strong <span>Organization</span> Skills.<span> Detail</span> Oriented. Looking to add <span>value</span> to a company with my <span>leadership</span> skills, <span>communication expertise</span>, and <span>desire to learn.</span>
         </p>
     </section>
